6c2ca105de8c41fac53da4ea3fb1715129967c5b
youssef shebl <63800183+youssefshibl@users.noreply.github.com>
2024-08-07 09:16:53 +0000

    set wait block to check if rabbitmq service is start or not

--- note-taking-service/entrypoint_worker.sh
+++ note-taking-service/entrypoint_worker.sh
@@ -4,6 +4,23 @@
 
 composer install --ignore-platform-reqs
 
+RABBITMQ_HOST="rabbitmq"
+RABBITMQ_PORT=5672
+
+echo "Checking if RabbitMQ is up and running on ${RABBITMQ_HOST}:${RABBITMQ_PORT}..."
+
+# Function to check if RabbitMQ is running
+check_rabbitmq() {
+    nc -z $RABBITMQ_HOST $RABBITMQ_PORT
+}
+
+# Loop until RabbitMQ is up
+while ! check_rabbitmq; do
+    echo "RabbitMQ is not available. Waiting..."
+    sleep 5  # Wait for 5 seconds before checking again
+done
+
+echo "RabbitMQ is up and running!"
 
 # Run the Laravel queue worker
 php /var/www/html/artisan queue:work
